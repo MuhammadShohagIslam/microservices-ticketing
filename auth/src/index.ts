059d381ae897768b68128c866e32d41b7fb34032
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-09-19 13:57:45 +0300

    setup ingress in the project

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,11 @@ const app = express();
 
 // middlware
 app.use(json());
-console.log(json())
+
+app.get("/api/users/currentuser", (req, res) => {
+    console.log("Hi There!");
+    res.send("ngix");
+});
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
