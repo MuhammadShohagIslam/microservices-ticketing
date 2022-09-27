b32ea88a39608d064a43a1f79cfad3a136ab7208
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-09-27 08:59:51 +0300

    Defining extra document properties

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -7,12 +7,25 @@ interface UserAttrs {
     password: string;
 }
 
-// An interface that describes the properties
-// that a User Model has
-interface UserModel extends Model<UserAttrs>{
-    build(attrs:UserAttrs): UserAttrs
+/*
+    => An interface that describes the properties
+    that a User Model has
+    => That describes what the entire collection of 
+    users or at least methods associated with the user
+    model
+*/
+interface UserModel extends Model<UserDoc>{
+    build(attrs:UserAttrs): UserDoc
 }
-const userSchema = new Schema<UserAttrs, UserModel>({
+
+// An interface that describes the properties
+// that a user document has
+interface UserDoc extends Document{
+    email: string,
+    password: string
+}
+
+const userSchema = new Schema({
     email: {
         type: String,
         required: true,
@@ -28,13 +41,7 @@ userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs)
 }
 
-const User = model<UserAttrs, UserModel>("Users", userSchema);
-
-User.build({
-    email: "a",
-    password:"22"
-})
-
+const User = model<UserDoc, UserModel>("Users", userSchema);
 
 // one way to fix type in new user
 // const buildUser = (attrs: UserAttrs) => {
